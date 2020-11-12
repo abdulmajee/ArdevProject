a3e716621fa2273fe30d357c0c942b0faf161c98
Abdul Majeed <abdulmajee@users.noreply.github.com>
2020-11-12 18:06:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 fetch('african.custom.geo.json')
   .then(response => response.json())
-  .then(data => console.log(data.features[10].geometry.coordinates));
+  .then(data =>
+    {
+      console.log(data.features[10].geometry.coordinates)
 
-
-  // document.addEventListener("mousemove", getMouseLocation(e));
-
-  // function getMouseLocation (event){
-  //   console.log(event.clientX, event.clientY)
-  // }
+    } );
